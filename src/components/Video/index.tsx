eb9ab0a4b7549803c72c9446c3c290a6a2cbae60
mason-at-pieces <mason@pieces.app>
2024-03-08 18:33:57 -0500

    feat(Video Component): Add support for youtube and mp4 videos

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,15 +1,32 @@
 const Video = ({
-  src
+  src,
+  type,
+  style
 }: {
   src: string
+  type: 'youtube' | 'mp4'
+  style?: React.CSSProperties
 }) => (
-  <iframe
-    width="100%"
-    height="500px"
-    src={src}
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowFullScreen={true}>
-  </iframe>
+  type === 'youtube' ? (
+    <iframe
+      width="100%"
+      height="500px"
+      src={src}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen={true}>
+    </iframe>
+  ) : type === 'mp4' ? (
+    <video
+      autoPlay={true}
+      loop={true}
+      muted={true}
+      playsInline={true}
+      width={'100%'}
+      style={style}
+    >
+      <source src={src} type="video/mp4" />
+    </video>
+  ): null
 );
 
 export default Video;
