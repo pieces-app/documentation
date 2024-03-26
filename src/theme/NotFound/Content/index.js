3437e45670bd6f9594be152cc4a1a773353b486d
mason-at-pieces <mason@pieces.app>
2024-03-26 18:49:50 -0400

    refactor(NotFound): simplify layout and content structure

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -6,63 +6,61 @@ import {MiniSpacer} from "@site/src/components/Spacers";
 
 export default function NotFoundContent({className}) {
   return (
-    <main className={clsx('container margin-vert--xl', className)}>
-      <div className="row">
-        <div className="col col--8 col--offset-2">
-          <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}>
-            <img
-              src={'/assets/404_image.webp'}
-              width={500}
-              alt={'404 - Page Not Found'}
-            />
-          </div>
+    <main className={'hero-container'}>
+      <div>
+        <h1 className="hero__title">You found a dead end!</h1>
+        <p>It seems like you've found a missing link. Don't worry, it happens to the best of us!</p>
 
-          <h1 className="hero__title">You found a dead end!</h1>
-          <p>It seems like you've found a missing link. Don't worry, it happens to the best of us!</p>
+        <hr/>
 
-          <hr />
+        <h2>Get Started with Pieces for Developers</h2>
+        <ul>
+          <li><a href={'/installation-getting-started/what-am-i-installing'}>What is Pieces for Developers?</a></li>
+          <li><a href={'https://youtube.com/@getpieces'}>Video Tutorials</a></li>
+          <li><a href={'/faq'}>FAQs</a></li>
+        </ul>
 
-          <h2>Get Started with Pieces for Developers</h2>
-          <ul>
-            <li><a href={'/installation-getting-started/what-am-i-installing'}>What is Pieces for Developers?</a></li>
-            <li><a href={'https://youtube.com/@getpieces'}>Video Tutorials</a></li>
-            <li><a href={'/faq'}>FAQs</a></li>
-          </ul>
+        <h2>Explore Plugins</h2>
+        <GridFlexRow type={'start'}>
+          <CTAButton
+            href={'/extensions-plugins/vscode'}
+            label={'VS Code Extension'}
+            icon={'/assets/vscode_logo.png'}
+            type={'secondary'}
+          />
+          <CTAButton
+            href={'/extensions-plugins/jetbrains'}
+            label={'JetBrains Plugin'}
+            icon={'/assets/jetbrains_logo.png'}
+            type={'secondary'}
+          />
+          <CTAButton
+            href={'/extensions-plugins/chrome'}
+            label={'Chrome Extension'}
+            icon={'/assets/chrome_logo.png'}
+            type={'secondary'}
+          />
+        </GridFlexRow>
 
-          <h2>Explore Plugins</h2>
-          <GridFlexRow type={'start'}>
-            <CTAButton
-              href={'/extensions-plugins/vscode'}
-              label={'VS Code Extension'}
-              icon={'/assets/vscode_logo.png'}
-              type={'secondary'}
-            />
-            <CTAButton
-              href={'/extensions-plugins/jetbrains'}
-              label={'JetBrains Plugin'}
-              icon={'/assets/jetbrains_logo.png'}
-              type={'secondary'}
-            />
-            <CTAButton
-              href={'/extensions-plugins/chrome'}
-              label={'Chrome Extension'}
-              icon={'/assets/chrome_logo.png'}
-              type={'secondary'}
-            />
-          </GridFlexRow>
+        <MiniSpacer/>
 
-          <MiniSpacer />
+        <h2>Need more help?</h2>
+        <ul>
+          <li>
+            <a href={'/support'}>Contact Support</a>
+          </li>
+        </ul>
+      </div>
 
-          <h2>Need more help?</h2>
-          <ul>
-            <li>
-              <a href={'/support'}>Contact Support</a>
-            </li>
-          </ul>
-        </div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        <img
+          src={'/assets/404_image.webp'}
+          width={500}
+          alt={'404 - Page Not Found'}
+        />
       </div>
     </main>
   );
