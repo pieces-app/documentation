086e55e8a6c5177774c46b353a60bf7801e28ecd
mason-at-pieces <mason@pieces.app>
2024-03-26 18:11:29 -0400

    feat(NotFoundContent): revamp 404 page layout and links

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -1,34 +1,74 @@
 import React from 'react';
 import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
-import Heading from '@theme/Heading';
+import CTAButton from "/src/components/CTAButton";
+import GridFlexRow from "@site/src/components/GridFlexRow";
+import {MiniSpacer} from "@site/src/components/Spacers";
+
 export default function NotFoundContent({className}) {
   return (
     <main className={clsx('container margin-vert--xl', className)}>
       <div className="row">
-        <div className="col col--6 col--offset-3">
-          <Heading as="h1" className="hero__title">
-            <Translate
-              id="theme.NotFound.title"
-              description="The title of the 404 page">
-              Page Not Found
-            </Translate>
-          </Heading>
-          <p>
-            <Translate
-              id="theme.NotFound.p1"
-              description="The first paragraph of the 404 page">
-              We could not find what you were looking for.
-            </Translate>
-          </p>
-          <p>
-            <Translate
-              id="theme.NotFound.p2"
-              description="The 2nd paragraph of the 404 page">
-              Please contact the owner of the site that linked you to the
-              original URL and let them know their link is broken.
-            </Translate>
-          </p>
+        <div className="col col--8 col--offset-2">
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <img
+              src={'/assets/404_image.webp'}
+              width={500}
+              alt={'404 - Page Not Found'}
+            />
+          </div>
+
+          <h1 className="hero__title">Oops! We goofed.</h1>
+          <p>It seems like you've found a missing link. Don't worry, it happens to the best of us!</p>
+
+          <hr />
+
+          <h2>Get Started with Pieces for Developers</h2>
+          <p>Here are some links to help you get started:</p>
+          <ul>
+            <li><a href={'/installation-getting-started/what-am-i-installing'}>What is Pieces for Developers?</a></li>
+            <li><a href={''}>Getting Started Guide</a></li>
+            <li><a href={'/docs/tutorials'}>Tutorials</a></li>you
+            <li><a href={'/faq'}>FAQs</a></li>
+          </ul>
+
+          <h2>Explore Plugins</h2>
+          <GridFlexRow type={'start'}>
+            <CTAButton
+              href={'/extensions-plugins/vscode'}
+              label={'VS Code Extension'}
+              icon={'/assets/vscode_logo.png'}
+              type={'secondary'}
+            />
+            <CTAButton
+              href={'/extensions-plugins/jetbrains'}
+              label={'JetBrains Plugin'}
+              icon={'/assets/jetbrains_logo.png'}
+              type={'secondary'}
+            />
+            <CTAButton
+              href={'/extensions-plugins/chrome'}
+              label={'Chrome Extension'}
+              icon={'/assets/chrome_logo.png'}
+              type={'secondary'}
+            />
+          </GridFlexRow>
+
+          <MiniSpacer />
+
+          <h2>Need more help?</h2>
+          <ul>
+            <li>
+              <a href={'/suppport'}>Contact Support</a>
+            </li>
+          </ul>
+          {/*<CTAButton*/}
+          {/*  label={'Contact Support'}*/}
+          {/*  href={'/support'}*/}
+          {/*  type={'secondary'}*/}
+          {/*/>*/}
         </div>
       </div>
     </main>
