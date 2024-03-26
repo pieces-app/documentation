1ab2318d8152d43859e6bee76ccca56453e0f993
mason-at-pieces <mason@pieces.app>
2024-03-25 20:10:34 -0400

    refactor: Change dropdown to docSidebar in nav config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -107,44 +107,44 @@ const config: Config = {
           label: 'Learn',
           position: 'right',
         },
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'sdksOverviewSidebar',
-        //   label: 'Build',
-        //   position: 'right',
-        // },
         {
-          type: 'dropdown',
+          type: 'docSidebar',
+          sidebarId: 'sdksOverviewSidebar',
           label: 'Build',
           position: 'right',
-          items: [
-            {
-              type: 'docSidebar',
-              sidebarId: 'sdksOverviewSidebar',
-              label: 'Overview',
-            },
-            // {
-            //   type: 'docSidebar',
-            //   sidebarId: 'typescriptSDKSidebar',
-            //   label: 'TypeScript SDK',
-            // },
-            {
-              type: 'docSidebar',
-              sidebarId: 'pythonSDKSidebar',
-              label: 'Python SDK',
-            },
-            {
-              type: 'docSidebar',
-              sidebarId: 'dartSDKSidebar',
-              label: 'Dart SDK',
-            },
-            {
-              type: 'docSidebar',
-              sidebarId: 'kotlinSDKSidebar',
-              label: 'Kotlin SDK',
-            }
-          ],
         },
+        // {
+        //   type: 'dropdown',
+        //   label: 'Build',
+        //   position: 'right',
+        //   items: [
+        //     {
+        //       type: 'docSidebar',
+        //       sidebarId: 'sdksOverviewSidebar',
+        //       label: 'Overview',
+        //     },
+        //     // {
+        //     //   type: 'docSidebar',
+        //     //   sidebarId: 'typescriptSDKSidebar',
+        //     //   label: 'TypeScript SDK',
+        //     // },
+        //     {
+        //       type: 'docSidebar',
+        //       sidebarId: 'pythonSDKSidebar',
+        //       label: 'Python SDK',
+        //     },
+        //     {
+        //       type: 'docSidebar',
+        //       sidebarId: 'dartSDKSidebar',
+        //       label: 'Dart SDK',
+        //     },
+        //     {
+        //       type: 'docSidebar',
+        //       sidebarId: 'kotlinSDKSidebar',
+        //       label: 'Kotlin SDK',
+        //     }
+        //   ],
+        // },
         {
           to: '/community',
           label: 'Community',
