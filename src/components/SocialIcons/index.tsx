d24028a1dfa12265f8948d1362689d0619c2a25a
mason-at-pieces <mason@pieces.app>
2024-04-10 18:14:00 -0400

    feat(SocialIcons): Add Discord link and standardize icon sizes

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -3,17 +3,20 @@ import GridFlexRow from "@site/src/components/GridFlexRow";
 const SocialIcons = () => {
   return (
     <GridFlexRow type={'evenly'}>
+      <a href="https://discord.gg/getpieces" target="_blank" rel="noreferrer">
+        <img width={50} src="/social/discord.png" alt="Discord" />
+      </a>
       <a href="https://twitter.com/getpieces" target="_blank" rel="noreferrer">
-        <img width={70} src="/social/twitter.webp" alt="Twitter" />
+        <img width={50} src="/social/twitter.webp" alt="Twitter" />
       </a>
       <a href="https://www.linkedin.com/company/getpieces" target="_blank" rel="noreferrer">
-        <img width={70} src="/social/linkedin.webp" alt="LinkedIn" />
+        <img width={50} src="/social/linkedin.webp" alt="LinkedIn" />
       </a>
       <a href="https://www.youtube.com/@getpieces" target="_blank" rel="noreferrer">
-        <img width={70} src="/social/youtube.webp" alt="YouTube" />
+        <img width={50} src="/social/youtube.webp" alt="YouTube" />
       </a>
       <a href="https://www.instagram.com/getpieces" target="_blank" rel="noreferrer">
-        <img width={70} src="/social/instagram.webp" alt="Instagram" />
+        <img width={50} src="/social/instagram.webp" alt="Instagram" />
       </a>
     </GridFlexRow>
   )
