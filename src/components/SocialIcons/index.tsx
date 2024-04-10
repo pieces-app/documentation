5812bedefa38a13ee3c23e3b34dc7beedcbbf1e8
mason-at-pieces <mason@pieces.app>
2024-04-10 18:08:15 -0400

    refactor: Update social icons and add MiniSpacer

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -1,42 +1,19 @@
 import GridFlexRow from "@site/src/components/GridFlexRow";
 
-// title: 'Socials',
-//   items: [
-//   {
-//     label: 'Discord',
-//     href: 'https://discord.gg/getpieces',
-//   },
-//   {
-//     label: 'Twitter',
-//     href: 'https://twitter.com/getpieces',
-//   },
-//   {
-//     label: 'LinkedIn',
-//     href: 'https://www.linkedin.com/company/getpieces',
-//   },
-//   {
-//     label: 'YouTube',
-//     href: 'https://www.youtube.com/@getpieces',
-//   },
-//   {
-//     label: 'Instagram',
-//     href: 'https://www.instagram.com/getpieces',
-//   }
-
 const SocialIcons = () => {
   return (
     <GridFlexRow type={'evenly'}>
       <a href="https://twitter.com/getpieces" target="_blank" rel="noreferrer">
-        <img src="/social/twitter.svg" alt="Twitter" />
+        <img width={70} src="/social/twitter.webp" alt="Twitter" />
       </a>
       <a href="https://www.linkedin.com/company/getpieces" target="_blank" rel="noreferrer">
-        <img src="/social/linkedin.svg" alt="LinkedIn" />
+        <img width={70} src="/social/linkedin.webp" alt="LinkedIn" />
       </a>
       <a href="https://www.youtube.com/@getpieces" target="_blank" rel="noreferrer">
-        <img src="/social/youtube.svg" alt="YouTube" />
+        <img width={70} src="/social/youtube.webp" alt="YouTube" />
       </a>
       <a href="https://www.instagram.com/getpieces" target="_blank" rel="noreferrer">
-        <img src="/social/instagram.svg" alt="Instagram" />
+        <img width={70} src="/social/instagram.webp" alt="Instagram" />
       </a>
     </GridFlexRow>
   )
