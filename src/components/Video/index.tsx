791b5169c8919d4478f3e8139f12665b9e0e3910
Mason Williams <mason@pieces.app>
2024-04-22 10:47:39 -0400

    refactor(docs): standardize video and image embeds

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,54 +1,55 @@
-const Video = ({
-  src,
-  type,
-  maxWidth,
-  alt,
-}: {
-  type: 'youtube'
-  src: {
-    youtube: string
-  }
-  maxWidth?: string
-  alt?: string
-} | {
-  type: 'local'
-  src: {
-    webm: string
-    mp4: string
-    ogg?: string
-  }
-  maxWidth?: string
-  alt?: string
-}) => (
-  type === 'youtube' ? (
-    <iframe
-      width="100%"
-      style={{
-        aspectRatio: '16 / 9',
-        maxWidth: maxWidth || '100%',
-      }}
-      src={src.youtube}
-      title={alt}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen={true}>
-    </iframe>
-  ) : type === 'local' ? (
-    <video
-      autoPlay={true}
-      loop={true}
-      muted={true}
-      playsInline={true}
-      width={'100%'}
-      style={{
-        borderRadius: '8px',
-        maxWidth: maxWidth || '100%',
-      }}
-    >
-      <source src={src.webm} title={alt} type="video/webm" />
-      <source src={src.mp4} title={alt} type="video/mp4" />
-      <source src={src.ogg} title={alt} type="video/ogg" />
-    </video>
-  ): null
-);
+type VideoProps = {
+  src: string;
+  type: 'gif' | 'youtube' | 'local';
+  maxWidth?: string;
+  alt?: string;
+}
+
+const Video = (props: VideoProps) => {
+  const { src, type, maxWidth, alt } = props;
+
+  return (
+    type === 'gif' ? (
+      <img
+        src={src}
+        alt={alt}
+        style={{
+          borderRadius: '8px',
+          maxWidth: maxWidth || '100%',
+        }}
+      />
+    ) :
+    type === 'youtube' ? (
+      <iframe
+        width="100%"
+        style={{
+          aspectRatio: '16 / 9',
+          maxWidth: maxWidth || '100%',
+        }}
+        src={src}
+        title={alt}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen={true}>
+      </iframe>
+    ) : type === 'local' ? (
+      <video
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        playsInline={true}
+        width={'100%'}
+        style={{
+          borderRadius: '8px',
+          maxWidth: maxWidth || '100%',
+        }}
+      >
+        <source src={`${src}.webm`} title={alt} type="video/webm"/>
+        <source src={`${src}.mp4`} title={alt} type="video/mp4"/>
+        <source src={`${src}.ogg`} title={alt} type="video/ogg"/>
+        Your browser does not support the video tag.
+      </video>
+    ) : null
+  )
+}
 
 export default Video;
