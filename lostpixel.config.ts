f4c63a1ce54963aaa3bd4ddd4e134edda00d8acd
Mason Williams <mason@pieces.app>
2024-04-25 18:15:36 -0400

    ci(lost-pixel): Update workflow and config for local test

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -4,8 +4,8 @@ export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'home' }],
     // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
-    baseUrl: 'http://172.17.0.1:3000',
-    // baseUrl: 'http://localhost:3000',
+    // baseUrl: 'http://172.17.0.1:3000',
+    baseUrl: 'http://localhost:3000',
   },
   // OSS mode
   generateOnly: true,
