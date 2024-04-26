ecf982acfc8d3c44eec36b55b6382b9c8889767e
Mason Williams <mason@pieces.app>
2024-04-26 16:14:10 -0400

    feat(lostpixel.config): Add new pages to pageShots config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,7 +2,13 @@ import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   pageShots: {
-    pages: [{ path: '/', name: 'home' }],
+    pages: [
+      { path: '/', name: 'home' },
+      { path: '/faq', name: 'faq' },
+      { path: '/community', name: 'community' },
+      { path: '/support', name: 'support' },
+      { path: '/early-access-program', name: 'early-access-program' },
+    ],
 
     baseUrl: 'http://172.17.0.1:3000',
   },
