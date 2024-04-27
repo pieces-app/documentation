c5fea8cf722864b002c334256439422e9027f910
Mason Williams <mason@pieces.app>
2024-04-27 15:57:47 -0400

    feat(lost-pixel): Automate page listing for tests

---
+++ cleanup/lost-pixel.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, '../docs/build/glossary');
+let pages = [];
+
+const getFiles = function(dirPath) {
+  fs.readdirSync(dirPath).forEach(function(file) {
+    let filePath = path.join(dirPath, file);
+    let stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      getFiles(filePath);
+    } else {
+      // Assuming you want to include all HTML files, adjust the condition as needed
+      if (path.extname(file) === '.md' || path.extname(file) === '.mdx') {
+        // Adjust the path format to match your requirements
+        let formattedPath = filePath.replace(directoryPath, '').replace('.html', '');
+        pages.push({
+          path: formattedPath.startsWith('/') ? formattedPath : '/' + formattedPath, // Ensure the path starts with '/'
+          name: formattedPath.replace('/', '').replace('.mdx', '').replace('md', '')
+        });
+      }
+    }
+  });
+};
+
+getFiles(directoryPath);
+
+// Wrap the pages array in an object with a "pages" key
+const pagesObject = { pages: pages };
+
+// Convert the pagesObject to JSON and save it
+fs.writeFileSync('static/lost-pixel.json', JSON.stringify(pagesObject, null, 2), 'utf8');
--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,14 +1,16 @@
 import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
+  // @ts-ignore
   pageShots: {
-    pages: [
-      { path: '/', name: 'home' },
-      { path: '/faq', name: 'faq' },
-      { path: '/community', name: 'community' },
-      { path: '/support', name: 'support' },
-      { path: '/early-access-program', name: 'early-access-program' },
-    ],
+    pagesJsonUrl: 'http://172.17.0.1:3000/lost-pixel.json',
+    // pages: [
+    //   { path: '/', name: 'home' },
+    //   { path: '/faq', name: 'faq' },
+    //   { path: '/community', name: 'community' },
+    //   { path: '/support', name: 'support' },
+    //   { path: '/early-access-program', name: 'early-access-program' },
+    // ],
 
     baseUrl: 'http://172.17.0.1:3000',
   },
