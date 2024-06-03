5e0c9ccb4df39388905e697d1b844e94a9ed9b6a
Mason Williams <mason@pieces.app>
2024-06-03 11:27:48 -0400

    feat: add Changelog link to navigation bar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,6 +98,11 @@ const config: Config = {
           position: 'right',
         },
         {
+          to: '/changelog',
+          label: 'Changelog',
+          position: 'right',
+        },
+        {
           type: 'dropdown',
           label: 'Community',
           position: 'right',
