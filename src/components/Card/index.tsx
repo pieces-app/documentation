e40c3a2cea27030c02e406f2cb0871a9f58ab2f8
Mason Williams <mason@pieces.app>
2024-06-14 12:39:09 -0400

    feat(docs): add Card component and update layout

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,46 @@
+import {MiniSpacer} from "@site/src/components/Spacers";
+import CTAButton from "@site/src/components/CTAButton";
+
+const Card = ({
+  title,
+  description,
+  href,
+  icon
+}: {
+  title: string,
+  description: string,
+  href: string,
+  icon: string
+}) => {
+  return (
+    <div className={'grid-card'}>
+      <div className="card-content">
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '10px'
+        }}>
+          <img
+            src={icon}
+            alt={title}
+            width={40}
+          />
+          <span style={{
+            fontSize: '1.5rem',
+            fontWeight: 'bold'
+          }}>{title}</span>
+        </div>
+        <MiniSpacer/>
+        <p>{description}</p>
+      </div>
+
+      <CTAButton
+        label={'Get Started'}
+        href={href}
+        type={'secondary'}
+      />
+    </div>
+  );
+}
+
+export default Card;
