2fc9bff5ebc6b6a5039cbb90e3d821f008a37fd0
Mason Williams <mason@pieces.app>
2024-07-11 16:23:11 -0400

    refactor(InstallButtons): simplify mobile detection logic

--- src/components/InstallButtons/index.tsx
+++ src/components/InstallButtons/index.tsx
@@ -81,22 +81,18 @@ const OSBasedButtons: React.FC = () => {
 
   return (
     <div>
-      {os != "Mobile" ? (
+      {os === "Mobile" ? (
         <CTAButton
           href="https://getpieces.typeform.com/to/aVQFTvpE?typeform-source=pieces.app"
           label={"Get Pieces Install Link"}
-          icon={
-              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-                <path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"/>
-              </svg>
-            }
-            type={'secondary'}
-            />
-            ) : (
-            <DesktopButtons />
-            )}
-        </div>
-        );
-      };
+          icon={<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"/></svg>}
+          type={'secondary'}
+        />
+      ) : (
+        <DesktopButtons />
+      )}
+    </div>
+  );
+};
 
-      export default OSBasedButtons
+export default OSBasedButtons
