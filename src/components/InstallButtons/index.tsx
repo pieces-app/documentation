d949f6585a11d0a1528a703b3b6bb0fbc47d9779
Mason Williams <mason@pieces.app>
2024-07-11 16:05:51 -0400

    docs(installation-guide): update tab and button layout

--- src/components/InstallButtons/index.tsx
+++ src/components/InstallButtons/index.tsx
@@ -3,6 +3,7 @@ import TabItem from "@theme/TabItem";
 import CTAButton from "@site/src/components/CTAButton";
 
 import React, { useEffect, useState } from "react";
+import GridFlexRow from "@site/src/components/GridFlexRow";
 
 // Helper function to detect the OS
 function detectOS() {
@@ -50,38 +51,27 @@ function useOS() {
 
 const DesktopButtons = () => {
   return (
-    <Tabs defaultValue={"macos"} values={[{label: 'macOS', value: 'macos'}, {label: 'Windows', value: 'windows'}, {label: 'Linux', value: 'linux'}]}>
-      <TabItem value="macos">
-        <p>Compatible with macOS 11.0 (Big Sur) and higher</p>
-        <CTAButton
-          href="/installation-getting-started/macos"
-          label={"Get Pieces for macOS"}
-          icon={'/assets/mac_os.png'}
-          iconDark={'/assets/mac_os_dark.png'}
-          type={'secondary'}
-        />
-      </TabItem>
-
-      <TabItem value="windows">
-        <p>Compatible with Windows 10 version 1809 and higher</p>
-        <CTAButton
-          href="/installation-getting-started/windows"
-          label={"Get Pieces for Windows"}
-          icon={'/assets/windows_logo_old.png'}
-          type={'secondary'}
-        />
-      </TabItem>
-
-      <TabItem value="linux">
-        <p>Compatible with Ubuntu 18 or higher (Currently in Beta)</p>
-        <CTAButton
-          href="/installation-getting-started/linux"
-          label={"Get Pieces for Linux"}
-          icon={'/assets/linux_logo.png'}
-          type={'secondary'}
-        />
-      </TabItem>
-    </Tabs>
+    <GridFlexRow type={'start'}>
+      <CTAButton
+        href="/installation-getting-started/windows"
+        label={"Get Pieces for Windows"}
+        icon={'/assets/windows_logo_old.png'}
+        type={'secondary'}
+      />
+      <CTAButton
+        href="/installation-getting-started/macos"
+        label={"Get Pieces for macOS"}
+        icon={'/assets/mac_os.png'}
+        iconDark={'/assets/mac_os_dark.png'}
+        type={'secondary'}
+      />
+      <CTAButton
+        href="/installation-getting-started/linux"
+        label={"Get Pieces for Linux"}
+        icon={'/assets/linux_logo.png'}
+        type={'secondary'}
+      />
+    </GridFlexRow>
   )
 }
 
@@ -91,18 +81,22 @@ const OSBasedButtons: React.FC = () => {
 
   return (
     <div>
-      {os === "Mobile" ? (
+      {os != "Mobile" ? (
         <CTAButton
           href="https://getpieces.typeform.com/to/aVQFTvpE?typeform-source=pieces.app"
-          label={"Get Pieces for Mobile"}
-          icon={'/assets/mobile_os.png'}
-          type={'secondary'}
-        />
-      ) : (
-        <DesktopButtons />
-      )}
-    </div>
-  );
-};
+          label={"Get Pieces Install Link"}
+          icon={
+              <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+                <path d="M24 21h-24v-18h24v18zm-23-16.477v15.477h22v-15.477l-10.999 10-11.001-10zm21.089-.523h-20.176l10.088 9.171 10.088-9.171z"/>
+              </svg>
+            }
+            type={'secondary'}
+            />
+            ) : (
+            <DesktopButtons />
+            )}
+        </div>
+        );
+      };
 
-export default OSBasedButtons
+      export default OSBasedButtons
